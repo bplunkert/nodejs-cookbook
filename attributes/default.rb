66731a66ea8cb429373bc409f0501f573e44e35a
Ben Plunkert <ben@init.me>
2014-06-05 12:03:14 -0700

    updating node version and checksums to 0.10.28 in attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,11 +23,10 @@ case node['platform_family']
   else
     default['nodejs']['install_method'] = 'source'
 end
-
-default['nodejs']['version'] = '0.10.26'
-default['nodejs']['checksum'] = 'ef5e4ea6f2689ed7f781355012b942a2347e0299da0804a58de8e6281c4b1daa'
-default['nodejs']['checksum_linux_x64'] = '305bf2983c65edea6dd2c9f3669b956251af03523d31cf0a0471504fd5920aac'
-default['nodejs']['checksum_linux_x86'] = '8fa2d952556c8b5aa37c077e2735c972c522510facaa4df76d4244be88f4dc0f'
+default['nodejs']['version'] = '0.10.28'
+default['nodejs']['checksum'] = 'abddc6441e0f208f6ed8a045e0293f713ea7f6dfb2d6a9a2024bf8b1b4617710'
+default['nodejs']['checksum_linux_x64'] = '5f41f4a90861bddaea92addc5dfba5357de40962031c2281b1683277a0f75932'
+default['nodejs']['checksum_linux_x86'] = '81ee7f30c35e1743790fd9ca47235bdec4a6c9d2b89a70f33c69e80008cbf422'
 default['nodejs']['checksum_linux_arm-pi'] = '561ec2ebfe963be8d6129f82a7d1bc112fb8fbfc0a1323ebe38ef55850f25517'
 default['nodejs']['dir'] = '/usr/local'
 default['nodejs']['npm'] = '1.4.4'
